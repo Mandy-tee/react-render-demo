8bef2334ae1e72b9953c8b219d7a4aa706e5ff10
Amanda Tagoe <nayork77@gmail.com>
2025-01-26 17:09:08 +0000

    Rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { UseReducerComponent } from './components/UseReducer/UseReducer';
 import UseState from './components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      <UseReducerComponent />
+      {/* <UseState /> */}
     </div>
   );
 }
